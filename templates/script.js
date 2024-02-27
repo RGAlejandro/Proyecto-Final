c1ad44ff452814897bab572497ceba91a4525168
RGAlejandro <rubiogemesalejandro@gmail.com>
2024-02-27 20:23:55 +0100

    Primer Commit

---
+++ templates/script.js
@@ -0,0 +1,8 @@
+function toggleUserMenu() {
+    var userMenu = document.getElementById("userMenu");
+    if (userMenu.style.display === "none") {
+        userMenu.style.display = "block";
+    } else {
+        userMenu.style.display = "none";
+    }
+}
